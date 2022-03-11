536b560d275fce84479b54aa40af2eba8bf56dad
Jp Venadas <jaypheevenadas18@gmail.com>
2022-03-11 15:08:25 +0800

    fully created an accessible json server

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import Addtask from "./components/Addtask"
 
 const App = () =>{
-   const [showAdd, setShowAdd] = useState(false)
-   const [tasks, setTasks] = useState([])
-      const addTask = ()=>{
-        setShowAdd(true)
-      }
-      const deleteTask = (id)=>{
-        setTasks(tasks.filter(task=> id !== task.id))
-      }
-      const togglereminder = (id)=>{
-        setTasks(tasks.map(task=>id === task.id? {...task,reminder: !task.reminder}: task))
-      }
-      const closeAdd = () =>{
-        setShowAdd(false)
-      }
-      const onSubmit = (task)=>{
-        
-         let newtask = { id: Math.floor(Math.random()*100000000), ...task}
-        setTasks([...tasks, newtask])
-        setShowAdd(false)
-      
-      }
+  const [showAdd, setShowAdd] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(()=>{
+     fetchTasks().then(result =>{
+       setTasks(result)
+     })
+  },[])
+
+  const fetchTasks = async () =>{
+    const res = await fetch('http://localhost:5000/tasks')
+    let data = res.json();
+   return data
+  } 
+
+  const addTask = ()=>{
+    setShowAdd(true)
+  }
+  const deleteTask = async(id)=>{
+    await fetch(`http://localhost:5000/tasks/${id}`,{method: 'DELETE'})
+    setTasks(tasks.filter(task=> id !== task.id))
+  }
+  const togglereminder = (id)=>{
+    setTasks(tasks.map(task=>id === task.id? {...task,reminder: !task.reminder}: task))
+  }
+  const closeAdd = () =>{
+    setShowAdd(false)
+  }
+  const onSubmit = async (task)=>{
+    const res = await fetch('http://localhost:5000/tasks',
+    {
+      method: 'POST',
+      headers: {'Content-type':'application/json'},
+      body: JSON.stringify(task)
+    })
+    let newtask = await res.json()
+    setTasks([...tasks, newtask])
+  }
   return(
     <div className="container">
       {showAdd? <Addtask onSubmit={onSubmit} onClose={closeAdd}/> : ''}
